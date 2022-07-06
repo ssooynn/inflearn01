8aef441ad5d1cfc752914329265957d8e47fa842
sooyeon lee <2sy0127@gmail.com>
2022-07-06 21:19:27 +0900

    0706

---
+++ inflearn/inflearn01/src/test/java/com/inflearn01/inflearn01/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.inflearn01.inflearn01.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+    }
+}
